a0a4f7f377c88f42fdbc64f105259d65b9e47c4a
ddigges <deborahgertrude_digges@intuit.com>
2017-08-21 12:23:02 +0530

    Add super usage

--- day1/js-features/inheritance.js
+++ day1/js-features/inheritance.js
@@ -2,13 +2,23 @@ class Animal {
     makeNoise() {
         
     }
+    
+    eat() {
+        console.log("Eating");
+    }
 }
 
 class Dog extends Animal {
     makeNoise() {
         console.log("bow bow");
     }
+    
+    eat() {
+        super.eat();
+        console.log("wuff wuff");
+    }
 }
 
 var doggie = new Dog();
 doggie.makeNoise();
+doggie.eat();
