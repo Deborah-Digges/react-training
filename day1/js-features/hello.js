2ba42fbf6415fcb3c7214faf5377abce386e375f
ddigges <deborahgertrude_digges@intuit.com>
2017-08-21 11:40:37 +0530

    Add arrays example

---
+++ day1/js-features/hello.js
@@ -0,0 +1,13 @@
+// run with node hello.js
+console.log("hello");
+
+// arrays
+var even = [2, 4, 6, 8];
+
+for(var i=0; i<even.length; ++i) {
+    console.log(even[i]);
+}
+
+var odd = [];
+odd.push(3);
+console.log(odd);
